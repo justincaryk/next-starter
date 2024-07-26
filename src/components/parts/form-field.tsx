d55df3b5d0fe6acd12a37395b8585eb93339889b
Justin Caryk <cary9983@gmail.com>
2024-07-26 11:50:52 -0700

    input refactor

--- src/components/parts/form-field.tsx
+++ src/components/parts/form-field.tsx
@@ -3,6 +3,7 @@ import { FieldError } from 'react-hook-form';
 
 import Input from './input';
 import Label from './label';
+import Password from './password';
 
 interface FormFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -15,7 +16,11 @@ const FormField = React.forwardRef(
     return (
       <div className="space-y-3">
         {label ? <Label text={label} htmlFor={name} /> : null}
-        <Input errors={errors} name={name} {...rest} ref={ref} />
+        {rest.type === 'password' ? (
+          <Password errors={errors} name={name} {...rest} ref={ref} />
+        ) : (
+          <Input errors={errors} name={name} {...rest} ref={ref} />
+        )}
         <div className="text-red-error" role="alert">
           {errors?.message}
         </div>
--- src/components/parts/input.tsx
+++ src/components/parts/input.tsx
@@ -1,67 +1,37 @@
 'use client';
 
-import classnames from 'classnames';
-import React, { InputHTMLAttributes, useState } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import { FieldError } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
 
-const baseInputStyles =
+export const baseInputStyles =
   'h-10 px-3 py-1.5 w-full text-gray-700 bg-white border border-solid rounded transition ease-in-out m-0 focus:outline-none bg-clip-padding';
-const cleanStyles = 'border-gray-300  focus:border-blue-md ';
-const errorStyles = 'border-red-error  focus:border-red-error ';
+export const cleanIputStyles = 'border-gray-300  focus:border-blue-md';
+export const errorInputStyles = 'border-red-error  focus:border-red-error';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   errors?: FieldError;
+  className?: string;
 }
 
 const Input = React.forwardRef(
-  ({ type, errors, ...rest }: InputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
-    const [revealed, setRevealed] = useState(false);
-
-    const togglePasswordReveal = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-      e.stopPropagation();
-      setRevealed(!revealed);
-    };
-
-    if (type === 'password') {
-      return (
-        <div
-          className={classnames({
-            'w-full flex justify-end items-center relative': true,
-          })}
-        >
-          <input
-            ref={ref}
-            id={rest.name}
-            aria-invalid={errors?.message ? 'true' : 'false'}
-            className={classnames({
-              [baseInputStyles]: true,
-              [cleanStyles]: !errors?.message,
-              [errorStyles]: errors?.message,
-            })}
-            {...rest}
-            type={revealed ? 'text' : 'password'}
-          />
-          <img
-            className="w-8 absolute right-2 cursor-pointer opacity-50"
-            src={revealed ? 'icons/eye-slash.svg' : 'icons/eye.svg'}
-            onClick={togglePasswordReveal}
-            alt={revealed ? 'hide password' : 'show password'}
-          />
-        </div>
-      );
-    }
+  (
+    { type, className = '', errors, ...rest }: InputProps,
+    ref: React.ForwardedRef<HTMLInputElement>,
+  ) => {
+    const classes = twMerge(
+      baseInputStyles,
+      errors?.message ? errorInputStyles : cleanIputStyles,
+      className,
+    );
 
     return (
       <input
         ref={ref}
         id={rest.name}
-        type="text"
+        type={type}
         aria-invalid={errors?.message ? 'true' : 'false'}
-        className={classnames({
-          [baseInputStyles]: true,
-          [cleanStyles]: !errors?.message,
-          [errorStyles]: errors?.message,
-        })}
+        className={classes}
         {...rest}
       />
     );
